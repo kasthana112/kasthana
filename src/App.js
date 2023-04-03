337ae2190afd11432e3c08999ecf4fa951af2677
kasthana112 <kanika.asthana@gmail.com>
2023-04-03 18:38:30 -0500

    updated navbar and added footer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import NavBar from './componets/NavBar';
+import Footer from './componets/Footer';
 import './App.css';
+
 
 function App() {
   return (
     <div className="App">
-      <h1>Navbar</h1>
+      <NavBar />
+      <Footer />
     </div>
   );
 }
---
+++ src/componets/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div>   <footer className='footer mt-auto py-3 bg-dark text-white'>
+    <div className='container'>Place sticky footer content here.</div>
+    
+  </footer>
+  </div>
+  )
+}
+
+export default Footer
