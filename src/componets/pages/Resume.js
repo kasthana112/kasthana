c801fc1fba5ac3244ce4abfb0d7fcfa8586bc7cd
kasthana112 <kanika.asthana@gmail.com>
2023-04-06 00:05:20 -0500

    added resume page

---
+++ src/componets/pages/Resume.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import resume from '../assets/resume.pdf';
+
+function Resume() {
+    return (
+        <div>
+            <h1>Resume</h1>
+            <a href={resume} target="_blank" rel="noopener noreferrer">Download Resume</a>
+        </div>
+    )
+}
+
+export default Resume;
