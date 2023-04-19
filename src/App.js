7b3bbea8d4134aa8cb05469217ee6fb0412a1c75
kasthana112 <kanika.asthana@gmail.com>
2023-04-18 22:18:40 -0500

    added routes on app.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,11 @@ import React from 'react';
 import Pageroutes  from './componets/PageRoutes';
 import NavBar from './componets/NavBar';
 import Footer from './componets/Footer';
+import About from './componets/pages/About';
+import Contact from './componets/pages/Contact';
+import Projects from './componets/pages/Projects';
+import Resume from './componets/pages/Resume';
+
 import './App.css';
 
 
