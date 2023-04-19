418ac8f0a20ee064959db29445eb4a048e2ddd85
kasthana112 <kanika.asthana@gmail.com>
2023-04-18 23:52:14 -0500

    updated app.js

--- src/App.js
+++ src/App.js
@@ -1,20 +1,33 @@
-import React from 'react';
-import Pageroutes  from './componets/PageRoutes';
+import React, { useState } from "react";
 import NavBar from './componets/NavBar';
 import Footer from './componets/Footer';
-import About from './componets/pages/About';
+import AboutMe from './componets/pages/About';
 import Contact from './componets/pages/Contact';
 import Projects from './componets/pages/Projects';
-import Resume from './componets/pages/Resume';
-import './App.js';
+
 
 
 function App() {
+  const [currentPage, setCurrentPage] = useState("About Me");
+
+  const renderPage = () => {
+      if (currentPage === "About Me") {
+          return <AboutMe />;
+      } else if (currentPage === "Projects") {
+          return <Projects />;
+      } else if (currentPage === "Contact") {
+          return <Contact />;
+      }
+  };
+
+  const handlePageChange = (page) => setCurrentPage(page);
+
   return (
-    <div className="App">
-      <NavBar />
-      <Footer />
-    </div>
+      <div>
+          <NavBar currentPage={currentPage} handlePageChange={handlePageChange} />
+          {renderPage()}
+          <Footer />
+      </div>
   );
 }
 
--- src/componets/PageRoutes.js
+++ src/componets/PageRoutes.js
@@ -5,27 +5,6 @@ import Contact from "./pages/Contact"
 import AboutMe from "./pages/About";
 import Projects from "./pages/Projects";
 
-export default function Portfolio() {
-    const [currentPage, setCurrentPage] = useState("About Me");
 
-    const renderPage = () => {
-        if (currentPage === "About Me") {
-            return <AboutMe />;
-        } else if (currentPage === "Projects") {
-            return <Project />;
-        } else if (currentPage === "Contact") {
-            return <Contact />;
-        }
-    };
 
-    const handlePageChange = (page) => setCurrentPage(page);
-
-    return (
-        <div>
-            <NavBar currentPage={currentPage} handlePageChange={handlePageChange} />
-            {renderPage()}
-            <Footer />
-        </div>
-    );
-}
 
